85774711d253e4da7fa0a57a78fead336cd8770c
Bardo <bardoespinosaa@gmail.com>
2023-07-06 10:46:51 -0600

    dataset added
    this dataset comes fro kaggle ,it is not mine

--- Pandas/pandas_0.py
+++ Pandas/pandas_0.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 
+
 #0
 #fruits = pd.DataFrame({"Apples":[30], "Bananas":[21]})
 #print(fruits)
@@ -18,5 +19,11 @@ liking = pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'],
 print(liking)
 """
 
+"""
+#3
 sales = pd.Series([30, 35, 40.2], index=['2015 Sales', '2016 Sales', '2017 Sales'], name='Product A')
 print(sales)
+"""
+
+fruit_prices = pd.read_csv("data\Fruit_Prices_2020.csv")
+print(fruit_prices.head() )
