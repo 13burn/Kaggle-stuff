5bccb1ec18e645c88e01a353974e72e6666e9d88
Bardo <bardoespinosaa@gmail.com>
2023-07-07 17:19:18 -0600

    Update pandas_6.py

--- Pandas/pandas_6.py
+++ Pandas/pandas_6.py
@@ -3,6 +3,11 @@ import pandas as pd
 canadian_youtube = pd.read_csv("data\CAvideos.csv")
 british_youtube = pd.read_csv("data\GBvideos.csv")
 
-print(canadian_youtube.shape)
-print(british_youtube.shape)
-print(pd.concat([canadian_youtube, british_youtube]))
+#print(canadian_youtube.shape)
+#print(british_youtube.shape)
+#print(pd.concat([canadian_youtube, british_youtube]))
+
+left =  canadian_youtube.set_index(["title", "trending_date"])
+right = british_youtube.set_index(["title", "trending_date"])
+#print(left)
+print(left.join(right, lsuffix="_CAN", rsuffix="_UK") )
